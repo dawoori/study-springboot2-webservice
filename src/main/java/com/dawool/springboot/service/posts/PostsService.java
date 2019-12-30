544031a1090c0622dc2289ad4ee48ace5239919e
dawoori <daul@kookmin.ac.kr>
2019-12-31 02:29:21 +0900

    Post 수정/조회 API, 생성시간/수정시

--- src/main/java/com/dawool/springboot/service/posts/PostsService.java
+++ src/main/java/com/dawool/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.dawool.springboot.service.posts;
 
+import com.dawool.springboot.domain.posts.Posts;
 import com.dawool.springboot.domain.posts.PostsRepository;
+import com.dawool.springboot.web.dto.PostsResponseDto;
 import com.dawool.springboot.web.dto.PostsSaveRequestDto;
+import com.dawool.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,19 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEnity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestsDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id="+ id));
+        posts.update(requestsDto.getTitle(), requestsDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id ="+ id));
+
+        return new PostsResponseDto(entity);
     }
 }
---
+++ src/main/java/com/dawool/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.dawool.springboot.web.dto;
+
+import com.dawool.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.content = entity.getAuthor();
+    }
+}
