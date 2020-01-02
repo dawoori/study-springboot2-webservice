5fb88e83aca8e1b9ad0f1a65f4befddacaa519df
dawoori <daul@kookmin.ac.kr>
2020-01-02 18:27:34 +0900

    조회/수정/삭제 기능

--- src/main/java/com/dawool/springboot/web/dto/PostsResponseDto.java
+++ src/main/java/com/dawool/springboot/web/dto/PostsResponseDto.java
@@ -1,22 +1,20 @@
 package com.dawool.springboot.web.dto;
 
 import com.dawool.springboot.domain.posts.Posts;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
-import java.time.LocalDateTime;
 
 @Getter
 public class PostsResponseDto {
 
     private Long id;
     private String title;
+    private String content;
     private String author;
-    private LocalDateTime modifiedDate;
 
     public PostsResponseDto(Posts entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
+        this.content = entity.getContent();
         this.author = entity.getAuthor();
-        this.modifiedDate = entity.getModifiedDate();
     }
 }
