60dbf348fe328193457317ba4292c0d4e5844a6b
dawoori <daul@kookmin.ac.kr>
2020-05-28 11:57:48 +0900

    db 재설정

--- src/main/java/com/dawool/springboot/Application.java
+++ src/main/java/com/dawool/springboot/Application.java
@@ -3,7 +3,7 @@ package com.dawool.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
+//
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
